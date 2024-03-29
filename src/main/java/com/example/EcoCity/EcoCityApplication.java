59e0869c8aebf807cc4fadad4d2a4eca129f821e
Danil <danil.tribushko@bk.ru>
2024-03-29 12:45:47 +0500

    Initial commit

---
+++ src/main/java/com/example/EcoCity/EcoCityApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcoCity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EcoCity/EcoCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EcoCity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
