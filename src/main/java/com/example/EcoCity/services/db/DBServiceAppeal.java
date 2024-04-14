00cf41abcacb7b5759d9d6082df6bcb068b3daa2
Danil <danil.tribushko@bk.ru>
2024-04-14 10:30:15 +0500

    14.04.2023
    Написал сервис для работы с обращениями в бд и его реализацию

---
+++ src/main/java/com/example/EcoCity/services/db/DBServiceAppeal.java
@@ -0,0 +1,12 @@
+package com.example.EcoCity.services.db;
+
+import com.example.EcoCity.models.entities.Appeal;
+
+/**
+ * @author Tribushko Danil
+ * @since 12.04.2024
+ * <p>
+ * Сервис для работы с обращениями в бд
+ */
+public interface DBServiceAppeal extends CrudDatabaseService<Appeal, Long> {
+}
